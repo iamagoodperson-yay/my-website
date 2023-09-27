36fb9d0e5f0c1abbb2dedeb4d9fb4cb4d88379dd
iamagoodperson-yay <kenzie.irawan11@gmail.com>
2023-09-27 21:54:07 +0800

    changed basically everything part 2

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.addEventListener('scroll', function() {
     var navbar = document.querySelector("nav");
-    console.log(navbar);
     if (this.window.pageYOffset > 750) {
         navbar.style.top = 0;
     } else {
